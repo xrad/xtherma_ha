xrad/xtherma_ha

commit 094b19faa73e7f45123d74d0ed2bae3559450271

fix units for out_backup and out_hp to WATT
closes #2

custom_components/xtherma_fp/sensor_descriptors.py
@@ -337,8 +337,7 @@ SENSOR_DESCRIPTIONS = [
     ),
     XtSensorEntityDescription(
         key="out_hp",
-        name="Leistungsabgabe Wärmepumpe (thermisch)",
-        native_unit_of_measurement=UnitOfPower.KILO_WATT,
+        native_unit_of_measurement=UnitOfPower.WATT,
         device_class=SensorDeviceClass.POWER,
         state_class=SensorStateClass.MEASUREMENT,
         icon=_icon_thermal_power,
@@ -367,8 +366,7 @@ SENSOR_DESCRIPTIONS = [
     ),
     XtSensorEntityDescription(
         key="out_backup",
-        name="Leistungsabgabe Zusatz-/Notheizung (thermisch)",
-        native_unit_of_measurement=UnitOfPower.KILO_WATT,
+        native_unit_of_measurement=UnitOfPower.WATT,
         device_class=SensorDeviceClass.POWER,
         state_class=SensorStateClass.MEASUREMENT,
         icon=_icon_thermal_power,

commit 3d84091808e268bb344515b0cf36573a2878b591

fix accidental change
094b19faa73e7f45123d74d0ed2bae3559450271 accidentally removed
descriptor names.

custom_components/xtherma_fp/sensor_descriptors.py
@@ -337,6 +337,7 @@ SENSOR_DESCRIPTIONS = [
     ),
     XtSensorEntityDescription(
         key="out_hp",
+        name="Leistungsabgabe Wärmepumpe (thermisch)",
         native_unit_of_measurement=UnitOfPower.WATT,
         device_class=SensorDeviceClass.POWER,
         state_class=SensorStateClass.MEASUREMENT,
@@ -366,6 +367,7 @@ SENSOR_DESCRIPTIONS = [
     ),
     XtSensorEntityDescription(
         key="out_backup",
+        name="Leistungsabgabe Zusatz-/Notheizung (thermisch)",
         native_unit_of_measurement=UnitOfPower.WATT,
         device_class=SensorDeviceClass.POWER,
         state_class=SensorStateClass.MEASUREMENT,